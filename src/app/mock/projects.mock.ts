2aa761946faa039b9b946cfd24be0cc137175654
niko <niko.dovidija@gmail.com>
2019-08-14 14:21:15 +0200

    Final check fix

--- src/app/mock/projects.mock.ts
+++ src/app/mock/projects.mock.ts
@@ -1,13 +1,13 @@
 import { Project } from '../model/project';
 
 export enum PType {
-    WEBAPP = "Web application",
-    MOBILEAPP = "Mobile application"
+    WEBAPP = 'Web application',
+    MOBILEAPP = 'Mobile application'
 }
 
 export enum SourceType {
-    GITHUB = "Github",
-    PRIVATEREPO = "Private repository",
+    GITHUB = 'Github',
+    PRIVATEREPO = 'Private repository',
 }
 export class ProjectType {
     type: PType;
@@ -21,7 +21,7 @@ export const projects: Project[] = [
         title: 'Balanse',
         descp: 'A colaborative project for transaction management',
         thumbnail: 'balanse.png',
-        stack: [ 'Angular 8', '.NET core','Mssql'],
+        stack: [ 'Angular 8', '.NET core', 'Mssql'],
         projectType:
             {
                 type: PType.WEBAPP,
@@ -34,7 +34,7 @@ export const projects: Project[] = [
         title: 'Visit Murska Sobota',
         descp: 'A tourism oriented web page made for a public institution',
         thumbnail: 'vms.png',
-        stack: ['Wordpress', 'Javascript','Mysql'],
+        stack: ['Wordpress', 'Javascript', 'Mysql'],
         projectType:
         {
             type: PType.WEBAPP,
@@ -47,7 +47,7 @@ export const projects: Project[] = [
         title: 'Playlistr',
         descp: 'A Vue.js and Laravel based webapp for music playback',
         thumbnail: '',
-        stack: ['Vue.js', 'Laravel','Mysql'],
+        stack: ['Vue.js', 'Laravel', 'Mysql'],
         projectType:
         {
             type: PType.WEBAPP,
@@ -70,8 +70,8 @@ export const projects: Project[] = [
     },
     {
         id: 4,
-        title: 'Nest js api',
-        descp: 'An example CRUD api with NESTJS',
+        title: 'Nest.js api',
+        descp: 'An example CRUD api with NESTJS framework, Node.js and MongoDb',
         thumbnail: '',
         stack: ['Node JS', 'Nest js', 'Typescript', 'MongoDb'],
         projectType:
@@ -81,5 +81,31 @@ export const projects: Project[] = [
             url: 'https://github.com/NikoDovidija/nestjs_crud_api',
         }
     },
-]
+    {
+        id: 5,
+        title: 'Express.js api',
+        descp: 'An example of a CRUD api using Node.js and Express with MongoDb',
+        thumbnail: '',
+        stack: ['Node JS', 'Nest js', 'Typescript', 'MongoDb'],
+        projectType:
+        {
+            type: PType.WEBAPP,
+            source: SourceType.GITHUB,
+            url: 'https://github.com/NikoDovidija/MongoExpressMongooseApi',
+        }
+    },
+    {
+        id: 6,
+        title: '.NET CORE webapp',
+        descp: 'A .NET core webapp project with basic boostrap frontend',
+        thumbnail: '',
+        stack: ['.NET CORE', 'Boostrap', 'C#', 'MSSQL'],
+        projectType:
+        {
+            type: PType.WEBAPP,
+            source: SourceType.GITHUB,
+            url: 'https://github.com/NikoDovidija/sportsstore',
+        }
+    },
+];
 
